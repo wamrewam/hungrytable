7c1586b081a7a30df050bde6252418318b7d0910
pjc <pjc@Pierre-Jeans-MacBook-Pro-2.local>
2012-12-19 17:05:38 +0000

    New config

--- lib/hungrytable/config.rb
+++ lib/hungrytable/config.rb
@@ -3,19 +3,20 @@ module Hungrytable
     extend self
 
     def partner_id
-      ENV['OT_PARTNER_ID'] || config_error('OT_PARTNER_ID')
+      ENV['OT_PARTNER_ID'] || TOPTABLE_PARTNER_ID || config_error('OT_PARTNER_ID')
     end
 
     def oauth_key
-      ENV['OT_OAUTH_KEY'] || config_error('OT_OAUTH_KEY')
+      ENV['OT_OAUTH_KEY'] || TOPTABLE_CONSUMER_KEY || config_error('OT_OAUTH_KEY')
     end
 
     def oauth_secret
-      ENV['OT_OAUTH_SECRET'] || config_error('OT_OAUTH_SECRET')
+      ENV['OT_OAUTH_SECRET'] || TOPTABLE_CONSUMER_SECRET || config_error('OT_OAUTH_SECRET')
     end
 
     def base_url
-      'https://secure.opentable.com/api/otapi_v2.ashx'
+      'https://toptable.co.uk/api/otapi_v2.ashx'
+      #'https://secure.opentable.com/api/otapi_v2.ashx'
     end
 
     private
